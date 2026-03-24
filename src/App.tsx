7ed648875c4d86e886fd5bbec475550fefd7c441
Jordan Plows <jordan@plows.ai>
2026-03-24 11:49:11 -0700

    finishing the landing

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ export default function App() {
       <div className="page">
         <main>
           <h1 className="hero">Nanowork</h1>
-          <p className="tagline">The future is automated.</p>
-          <p className="tagline">Your income should be too.</p>
+          <p className="tagline">
+            The future is automated. Your income should be too.
+          </p>
         </main>
         <footer>© Nanowork, Inc 2026</footer>
       </div>
