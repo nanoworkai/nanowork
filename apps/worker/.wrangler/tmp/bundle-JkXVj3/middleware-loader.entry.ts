34d7ef59153324859c586b9ae7c70ddeb95fe355
Jordan Plows <jordan@plows.ai>
2026-05-25 14:04:27 -0700

    updating the cf pipeline

--- apps/worker/.wrangler/tmp/bundle-JkXVj3/middleware-loader.entry.ts
+++ apps/worker/.wrangler/tmp/bundle-JkXVj3/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/jordan/Dev/nanowork-web/apps/worker/.wrangler/tmp/bundle-GS4SUW/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/jordan/Dev/nanowork-web/apps/worker/.wrangler/tmp/bundle-JkXVj3/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/jordan/Dev/nanowork-web/apps/worker/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/jordan/Dev/nanowork-web/apps/worker/.wrangler/tmp/bundle-GS4SUW/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/jordan/Dev/nanowork-web/apps/worker/.wrangler/tmp/bundle-JkXVj3/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/jordan/Dev/nanowork-web/apps/worker/.wrangler/tmp/bundle-GS4SUW/middleware-insertion-facade.js";
+export * from "/Users/jordan/Dev/nanowork-web/apps/worker/.wrangler/tmp/bundle-JkXVj3/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
